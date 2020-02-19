6de1890458339b5ee76df427b7888f5cc33932df
iamtash <natashachernis@gmail.com>
2020-02-19 10:22:53 -0600

    render SongList and SongDetail components in grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
+import SongList from './SongList'
+import SongDetail from './SongDetail'
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div className="ui grid">
+            <div className="ui row">
+                <div className="eight wide column">
+                    <SongList />
+                </div>
+                <div className="eight wide column">
+                    <SongDetail />
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
